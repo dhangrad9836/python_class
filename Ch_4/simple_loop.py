cd1b804b0da0b0d2b282acb01932916520f59c67
Darren Dhanpat <darren.dhanpat@yahoo.com>
2023-10-02 09:52:43 -0400

    updateing ch4

---
+++ Ch_4/simple_loop.py
@@ -0,0 +1,15 @@
+# This program demonstrates a simple for loop
+# That uses a list of numbers
+
+print('I will display hello 5 times')
+for num in range(5):
+    print('Hello')
+
+print('I will display the numbers 1 through 5 using range function')
+for num in range(1,6):
+    print(num)
+
+print('Range function with step value')
+# The step value skips over tha
+for num in range(1, 10, 2):
+    print(num)
--- Ch_4/temperature.py
+++ Ch_4/temperature.py
@@ -5,3 +5,18 @@
 MAX_TEMP = 102.5
 
 # Get the substance's temperature
+temperature = float(input("Enter the substance's Celsius temperature: "))
+
+# As lons as necessary, instruct the user to
+# adjust the thermostat.
+while temperature > MAX_TEMP:
+    print('The temperature is too high.')
+    print('Turn the thermostat down and wait')
+    print('5 minutes. Then take the temperature')
+    print('again and enter it.')
+    temperature = float(input('Enter the new Celsius temperature.   '))
+
+# Remind the user to check the termperature again
+# in 15 minutes.
+print('The temperature is acceptable.')
+print('Check it again in 15 minutes.')
