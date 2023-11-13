f5fbeb0af5131515768405a7ede5ca6290b9f2e5
Darren Dhanpat <darren.dhanpat@yahoo.com>
2023-11-12 19:06:08 -0500

    updating week 11 HW

--- Homework/week_11_hw/random_prime_number.py
+++ Homework/week_11_hw/random_prime_number.py
@@ -1,21 +1,26 @@
 import random
 
+def random_gen():
+        # Get a random number
+        return random.randint(1, 1000)
+
+def is_prime(num):
+    for i in range(2, num):
+        if(num % i) == 1:
+            return "Prime number"
+            break
+        else:
+            return "not prime"
+            break
+
+
 def main():
     # have user enter how many number to generate
-    userInput = input("Enter a number: ")
-    while userInput != 0:
-        if ord(userInput) not in range(48,58):
-            print("Not a number\n")
-            userInput = input("Enter a valid input: ")
-        else:
-            pass
+    userInput = int(input("Enter how many numbers to check if prime: "))
+    for i in range(userInput):
+        num = random_gen()
+        print(f'{num} is {is_prime(num)}')
 
-    for count in range(userInput):
-        # Get a random number
-        number = random.randint(1, 100)
-
-        # Display the number
-        print(f'Number {count + 1} is {number}.')
 
 # Call the main function
 main()
