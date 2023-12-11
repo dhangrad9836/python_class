1c451aa1c2ceb4172437d7a8f316e1681cedb911
Darren Dhanpat <darren.dhanpat@yahoo.com>
2023-12-11 10:18:55 -0500

    adding ch 7 for List methods

---
+++ Ch_7/list_slice.py
@@ -0,0 +1,8 @@
+days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+
+#This will include on the index at 2 and the item before the index 5
+mid_days = days[2:5]
+print(mid_days)
+
+numbers = [1, 2, 3, 4, 5]
+print(numbers[:3]) #[1, 2, 3]
